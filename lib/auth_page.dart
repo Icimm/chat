52d5ed22dc5967843f788787294c4ca3790f4b6e
Icimm <icarobellemskr@hotmail.com>
2023-06-01 02:25:59 -0300

    add new dependecies and new commands

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -1,18 +1,46 @@
+import 'dart:html';
+
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/components/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
-  const AuthPage
-({super.key});
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool __isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => __isLoading = true);
+    print('AuthPage...');
+    print(formData.email);
+    setState(() => __isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      body: Stack(
+        children: [
+          Center(
+              child: SingleChildScrollView(
+            child: AuthForm(onSubmit: _handleSubmit),
+           )
+          ),
+          if(__isLoading) Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+            ),
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -2,7 +2,12 @@ import 'package:chat/components/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    Key? key,
+    required this.onSubmit,
+  }) : super(key: key);
 
   @override
   State<AuthForm> createState() => _AuthFormState();
@@ -15,6 +20,8 @@ class _AuthFormState extends State<AuthForm> {
   void _submit() {
     final isValid = _FormKey.currentState?.validate() ?? false;
     if (!isValid) return;
+
+    widget.onSubmit(_FormData);
   }
 
   @override
